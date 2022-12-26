6b1bfd77c775b05023285e08dc568a0b64a3c2e2
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2022-12-26 15:06:39 +0200

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,18 @@ export class App extends Component {
   state = {
     isBtnClick: false,
   };
+
+  componentDidMount() {
+    const isBtnClick = localStorage.getItem('isBtnClick') === 'true';
+    this.setState({ isBtnClick });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.isBtnClick !== this.setState.isBtnClick) {
+      localStorage.setItem('isBtnClick', this.state.isBtnClick);
+    }
+  }
+
   onBtnClick = () => {
     this.setState(prevState => {
       return { isBtnClick: !prevState.isBtnClick };
